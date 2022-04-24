b560a718466e8f3056e68ab73d484cf7b03f4643
linariii <hsk.de@web.de>
2022-04-24 16:01:00 +0200

    add base action

---
+++ streamdeck-weather/Actions/ActionBase.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using BarRaider.SdTools;
+using Newtonsoft.Json.Linq;
+using Weather.Backend.Models;
+
+namespace Weather.Actions
+{
+    public abstract class ActionBase : PluginBase
+    {
+        private protected readonly GlobalSettings _globalSettings;
+        protected ActionBase(ISDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+            _globalSettings = GlobalSettings.CreateDefaultSettings();
+            GlobalSettingsManager.Instance.RequestGlobalSettings();
+        }
+
+        public override async void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload)
+        {
+            Logger.Instance.LogMessage(TracingLevel.INFO, "ReceivedGlobalSettings");
+            if (payload.Settings != null && payload.Settings.Count > 0)
+            {
+                Logger.Instance.LogMessage(TracingLevel.INFO, $"ReceivedGlobalSettings: {payload.Settings}");
+                var settings = payload.Settings.ToObject<GlobalSettings>();
+                if (settings != null && _globalSettings != null)
+                {
+                    var updated = false;
+                    if (settings.ApiKey != _globalSettings.ApiKey)
+                    {
+                        _globalSettings.ApiKey = settings.ApiKey;
+                        updated = true;
+                    }
+
+                    await SaveGlobalSettings(updated);
+                }
+            }
+        }
+
+        private async Task SaveGlobalSettings(bool triggerDidReceiveGlobalSettings = true)
+        {
+            if (_globalSettings != null)
+            {
+                Logger.Instance.LogMessage(TracingLevel.INFO, $"SaveGlobalSettings: {JObject.FromObject(_globalSettings)}");
+                await Connection.SetGlobalSettingsAsync(JObject.FromObject(_globalSettings), triggerDidReceiveGlobalSettings);
+            }
+        }
+
+        public override void Dispose()
+        {
+            Logger.Instance.LogMessage(TracingLevel.INFO, "Destructor called");
+        }
+
+        public override void KeyPressed(KeyPayload payload) { }
+
+        public override void KeyReleased(KeyPayload payload) { }
+
+        private protected string GetConditonIconPath(CurrentWeatherResult data)
+        {
+            if (string.IsNullOrWhiteSpace(data?.Current?.Condition?.Icon))
+                return null;
+
+            var assemblyLocation = Assembly.GetEntryAssembly()?.Location;
+            if (string.IsNullOrWhiteSpace(assemblyLocation))
+                return null;
+
+            var currentPath = Path.GetDirectoryName(assemblyLocation);
+
+            var index = data.Current.Condition.Icon.IndexOf("/weather/", StringComparison.Ordinal);
+            var iconSubPath = data.Current.Condition.Icon.Substring(index).Replace("/", "\\");
+
+            return $"{currentPath}{Path.DirectorySeparatorChar}Images{iconSubPath}";
+        }
+
+        private protected async Task DrawWeatherKeyImage(bool showTitle, string title, string data, string iconPath)
+        {
+            if (string.IsNullOrWhiteSpace(data) || string.IsNullOrWhiteSpace(iconPath))
+                return;
+
+            if (showTitle && string.IsNullOrWhiteSpace(title))
+                return;
+
+            try
+            {
+                using (var bmp = Tools.GenerateGenericKeyImage(out Graphics graphics))
+                {
+                    var height = bmp.Height;
+                    var width = bmp.Width;
+
+                    var fontDefault = new Font("Verdana", 20, FontStyle.Bold, GraphicsUnit.Pixel);
+                    var fontCurrency = new Font("Verdana", 28, FontStyle.Bold, GraphicsUnit.Pixel);
+
+                    var fgBrush = new SolidBrush(Color.White);
+                    float stringWidth;
+
+                    // Top title
+                    if (showTitle)
+                    {
+                        stringWidth = graphics.GetTextCenter(title, width, fontDefault);
+                        var fontSizeDefault = graphics.GetFontSizeWhereTextFitsImage(title, width, fontDefault, 8);
+                        fontDefault = new Font(fontDefault.Name, fontSizeDefault, fontDefault.Style, GraphicsUnit.Pixel);
+                        graphics.DrawAndMeasureString(title, fontDefault, fgBrush, new PointF(stringWidth, 5));
+                    }
+
+                    // Background
+                    if (!string.IsNullOrWhiteSpace(iconPath) && File.Exists(iconPath))
+                    {
+                        var backgroundImagePos = showTitle ? 20 : 5;
+                        var backgroundImage = Image.FromFile(iconPath);
+                        graphics.DrawImage(backgroundImage, 27, backgroundImagePos, 90, 90);
+                        backgroundImage.Dispose();
+                    }
+
+                    //temp
+                    var fontSizeTemp = graphics.GetFontSizeWhereTextFitsImage(data, width, fontCurrency, 20);
+                    fontCurrency = new Font(fontCurrency.Name, fontSizeTemp, fontCurrency.Style, GraphicsUnit.Pixel);
+                    stringWidth = graphics.GetTextCenter(data, width, fontCurrency);
+                    var tempPos = showTitle ? 111 : 105;
+                    graphics.DrawAndMeasureString(data, fontCurrency, fgBrush, new PointF(stringWidth, tempPos));
+
+                    await Connection.SetImageAsync(bmp);
+                    graphics.Dispose();
+                    fontDefault.Dispose();
+                    fontCurrency.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, $"{GetType()} Error drawing data {ex}");
+            }
+        }
+    }
+}
--- streamdeck-weather/Actions/CurrentWeather.cs
+++ streamdeck-weather/Actions/CurrentWeather.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Drawing;
-using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 using BarRaider.SdTools;
 using Newtonsoft.Json;
@@ -12,11 +9,10 @@ using Weather.Backend.Models;
 namespace Weather.Actions
 {
     [PluginActionId("com.linariii.weather.current")]
-    public class CurrentWeather : PluginBase
+    public class CurrentWeather : ActionBase
     {
         private const int FetchCooldownSec = 900; // 15 min
         private readonly PluginSettings _settings;
-        private readonly GlobalSettings _globalSettings;
 
         private class PluginSettings
         {
@@ -49,30 +45,15 @@ namespace Weather.Actions
             if (payload.Settings == null || payload.Settings.Count == 0)
             {
                 _settings = PluginSettings.CreateDefaultSettings();
-                _globalSettings = GlobalSettings.CreateDefaultSettings();
             }
             else
             {
                 Logger.Instance.LogMessage(TracingLevel.INFO, $"Settings: {payload.Settings}");
-                _globalSettings = GlobalSettings.CreateDefaultSettings();
                 _settings = payload.Settings.ToObject<PluginSettings>();
                 if (_settings != null)
                     _settings.LastRefresh = DateTime.MinValue;
             }
-            GlobalSettingsManager.Instance.RequestGlobalSettings();
         }
-
-        public override void Dispose()
-        {
-            Logger.Instance.LogMessage(TracingLevel.INFO, $"Destructor called");
-        }
-
-        public override void KeyPressed(KeyPayload payload)
-        {
-            Logger.Instance.LogMessage(TracingLevel.INFO, "Key Pressed");
-        }
-
-        public override void KeyReleased(KeyPayload payload) { }
 
         public override async void OnTick()
         {
@@ -102,7 +83,7 @@ namespace Weather.Actions
                         _settings.Data = data;
                         _settings.LastRefresh = DateTime.Now;
                         await SaveSettings();
-                        await Draw();
+                        await DrawKeyImage();
                     }
                 }
                 catch (Exception ex)
@@ -113,66 +94,20 @@ namespace Weather.Actions
             }
         }
 
-        private async Task Draw()
+        private async Task DrawKeyImage()
         {
-            Logger.Instance.LogMessage(TracingLevel.DEBUG, GetIconPath());
-            try
-            {
-                using (Bitmap bmp = Tools.GenerateGenericKeyImage(out Graphics graphics))
-                {
-                    var height = bmp.Height;
-                    var width = bmp.Width;
+            var showTitle = !string.IsNullOrWhiteSpace(_settings.DisplayName) && _settings.DisplayName == "1";
+            var title = !string.IsNullOrWhiteSpace(_settings.Data.Location?.Name)
+                ? _settings.Data.Location?.Name
+                : _settings.City;
 
-                    var fontDefault = new Font("Verdana", 20, FontStyle.Bold, GraphicsUnit.Pixel);
-                    var fontCurrency = new Font("Verdana", 28, FontStyle.Bold, GraphicsUnit.Pixel);
+            var data = !string.IsNullOrWhiteSpace(_settings.Unit) && _settings.Unit == "f"
+                ? $"{Math.Round(_settings.Data.Current.TempF, 0)} °F"
+                : $"{Math.Round(_settings.Data.Current.TempC, 0)} °C";
 
-                    var fgBrush = new SolidBrush(Color.White);
-                    var stringWidth = graphics.GetTextCenter(_settings.City, width, fontDefault);
-                    var showTitle = !string.IsNullOrWhiteSpace(_settings.DisplayName) && _settings.DisplayName == "1";
+            var iconPath = GetConditonIconPath(_settings.Data);
 
-                    // Top title
-                    if (showTitle)
-                    {
-                        var title = !string.IsNullOrWhiteSpace(_settings.Data.Location?.Name)
-                            ? _settings.Data.Location?.Name
-                            : _settings.City;
-
-                        var fontSizeDefault = graphics.GetFontSizeWhereTextFitsImage(title, width, fontDefault, 8);
-                        fontDefault = new Font(fontDefault.Name, fontSizeDefault, fontDefault.Style, GraphicsUnit.Pixel);
-                        graphics.DrawAndMeasureString(_settings.City, fontDefault, fgBrush, new PointF(stringWidth, 5));
-                    }
-
-                    // Background
-                    var iconPath = GetIconPath();
-                    if (!string.IsNullOrWhiteSpace(iconPath) && File.Exists(iconPath))
-                    {
-                        var backgroundImagePos = showTitle ? 20 : 5;
-                        var backgroundImage = Image.FromFile(iconPath);
-                        graphics.DrawImage(backgroundImage, 27, backgroundImagePos, 90, 90);
-                        backgroundImage.Dispose();
-                    }
-
-                    //temp
-                    var currStr = !string.IsNullOrWhiteSpace(_settings.Unit) && _settings.Unit == "f"
-                        ? $"{Math.Round(_settings.Data.Current.TempF, 0)} °F"
-                        : $"{Math.Round(_settings.Data.Current.TempC, 0)} °C";
-
-                    var fontSizeTemp = graphics.GetFontSizeWhereTextFitsImage(currStr, width, fontCurrency, 20);
-                    fontCurrency = new Font(fontCurrency.Name, fontSizeTemp, fontCurrency.Style, GraphicsUnit.Pixel);
-                    stringWidth = graphics.GetTextCenter(currStr, width, fontCurrency);
-                    var tempPos = showTitle ? 111 : 105;
-                    graphics.DrawAndMeasureString(currStr, fontCurrency, fgBrush, new PointF(stringWidth, tempPos));
-
-                    await Connection.SetImageAsync(bmp);
-                    graphics.Dispose();
-                    fontDefault.Dispose();
-                    fontCurrency.Dispose();
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Instance.LogMessage(TracingLevel.ERROR, $"{GetType()} Error drawing data {ex}");
-            }
+            await DrawWeatherKeyImage(showTitle, title, data, iconPath);
         }
 
         public override async void ReceivedSettings(ReceivedSettingsPayload payload)
@@ -189,57 +124,10 @@ namespace Weather.Actions
             }
         }
 
-        public override async void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload)
-        {
-            Logger.Instance.LogMessage(TracingLevel.INFO, $"ReceivedGlobalSettings");
-            if (payload.Settings != null && payload.Settings.Count > 0)
-            {
-                Logger.Instance.LogMessage(TracingLevel.INFO, $"ReceivedGlobalSettings: {payload.Settings}");
-                var settings = payload.Settings.ToObject<GlobalSettings>();
-                if (settings != null && _globalSettings != null)
-                {
-                    var updated = false;
-                    if (settings.ApiKey != _globalSettings.ApiKey)
-                    {
-                        _globalSettings.ApiKey = settings.ApiKey;
-                        updated = true;
-                    }
-
-                    await SaveGlobalSettings(updated);
-                }
-            }
-        }
-
-        private string GetIconPath()
-        {
-            if (string.IsNullOrWhiteSpace(_settings?.Data?.Current?.Condition?.Icon))
-                return null;
-
-            var assemblyLocation = Assembly.GetEntryAssembly()?.Location;
-            if (string.IsNullOrWhiteSpace(assemblyLocation))
-                return null;
-
-            var currentPath = Path.GetDirectoryName(assemblyLocation);
-
-            var index = _settings.Data.Current.Condition.Icon.IndexOf("/weather/", StringComparison.Ordinal);
-            var iconSubPath = _settings.Data.Current.Condition.Icon.Substring(index).Replace("/", "\\");
-
-            return $"{currentPath}{Path.DirectorySeparatorChar}Images{iconSubPath}";
-        }
-
         private async Task SaveSettings()
         {
             Logger.Instance.LogMessage(TracingLevel.INFO, $"SaveSettings: {JObject.FromObject(_settings)}");
             await Connection.SetSettingsAsync(JObject.FromObject(_settings));
-        }
-
-        private async Task SaveGlobalSettings(bool triggerDidReceiveGlobalSettings = true)
-        {
-            if (_globalSettings != null)
-            {
-                Logger.Instance.LogMessage(TracingLevel.INFO, $"SaveGlobalSettings: {JObject.FromObject(_globalSettings)}");
-                await Connection.SetGlobalSettingsAsync(JObject.FromObject(_globalSettings), triggerDidReceiveGlobalSettings);
-            }
         }
     }
 }
